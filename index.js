190ddc6ee1da126bbf096272b729cd6a82bf8f1e
fahimmutashir23 <taslimit23@gmail.com>
2023-10-19 01:13:16 +0600

    added get operation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const data = require("./data.json")
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config()
 const app = express();
@@ -14,7 +15,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pyhg6t2.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,7 +27,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const fashionCollection = client.db("fashionDB").collection("fashion");
+    // const fashionCollection = client.db("fashionDB").collection("fashion");
    
+    app.get("/fashionsBrand", (req, res) => {
+        res.send(data)
+    })
+
+    app.get("/fashions", async(req, res) => {
+        const cursor = fashionCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.post("/fashions", async(req, res) => {
+        const data = req.body;
+        const result = await fashionCollection.insertOne(data);
+        res.send(result)
+    })
+
     
 
     await client.db("admin").command({ ping: 1 });
@@ -41,7 +60,7 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res)=> {
-    res.send("surver is running successfully")
+    res.send("fashion Den surver is running successfully")
 })
 
 app.listen(port, () => {
